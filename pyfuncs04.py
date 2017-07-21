9d4dfead087272209600e6d8d504b92884a38692
Curt S <schurgersc1@gmail.com>
2017-07-20 20:01:27 -0700

    added py files and updated

---
+++ pyfuncs04.py
@@ -0,0 +1,18 @@
+# pyfuncs04.py
+# stubs for some sample functions
+
+def first(collection):
+    ''' first item of a string, list, tuple, or other indexable thing '''
+    return collection[0]
+
+def rest(collection):
+    ''' all but the first item of a indexable thing (str, list, tuple) '''
+    return collection[1:]
+
+def last(collection):
+    ''' last item of an indexable thing '''
+    return "stub"
+
+def allButLast(collection):
+    ''' all but the last item of an indexable thing '''
+    return "stub"
